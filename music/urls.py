7b087e6417e8615e4281aa27e9f8aee5749c1958
Yaroslav Pavliuk <ddtrankill@gmail.com>
2017-02-06 00:03:26 +0200

    CRUD added

--- music/urls.py
+++ music/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
+app_name = 'music'
 
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='create_album'),
+    url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='update_album'),
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='delete_album'),
 ]
